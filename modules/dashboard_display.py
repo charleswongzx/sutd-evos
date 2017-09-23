36e4c0631d6e7308f2ebf12d1f42d20d403e82ed
charleswongzx <charlescrinkle@gmail.com>
2017-09-23 16:46:23 +0800

    completed dashboard layout

--- modules/dashboard_display.py
+++ modules/dashboard_display.py
@@ -14,27 +14,33 @@ Config.set('graphics', 'width', 800)
 
 from kivy.app import App
 
-from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivy.properties import NumericProperty
+from kivy.properties import BooleanProperty
+from kivy.properties import StringProperty
+
+
+from kivy.core.window import Window
+Window.clearcolor = (0.1, 0.1, 0.1, 1)
 
 
 # Global vars
-lap_counter = 0
+lap_counter = 1
+num_laps = 10
 
 # net_status
-signal_str = 0
+net_signal = 0
 
 # mode_control
 evos_mode = 0
 
 # eng_status
-fuel_flow = 0
-running = 0
-speed = 0
-temp = 0
-rpm = 0
+eng_running = 0
+eng_fuel_flow = 0
+eng_temp = 0
+eng_rpm = 0
 
+veh_speed = 0
 
 # LCM handlers
 def eng_status_handler(channel, data):
@@ -71,18 +77,37 @@ mode_control_sub = lc.subscribe("mode_control", mode_control_handler)
 
 # Creating widgets
 class VehicleModeWidget(BoxLayout):
+    if evos_mode == 0:
+        mode = StringProperty('DEV')
+        color = (0.3, 0.3, 0.3, 1)
+    elif evos_mode == 1:
+        mode = StringProperty('SHOWROOM')
+        color = (0.1, 0.1, 1, 1)
+    elif evos_mode == 2:
+        mode = StringProperty('RACE')
+        color = (1, 0, 0, 1)
+
     pass
 
 
 class LapWidget(BoxLayout):
+    current_lap = StringProperty(str(lap_counter))
+    max_lap = StringProperty(str(num_laps))
     pass
 
 
 class IgnitionWidget(BoxLayout):
+    if eng_running:
+        running = StringProperty('RUNNING')
+        color = (1, 0.1, 0.1, 1)
+    else:
+        running = StringProperty('OFF')
+        color = (0.3, 0.3, 0.3, 1)
     pass
 
 
 class RPMWidget(BoxLayout):
+    rpm = NumericProperty(eng_rpm)
     pass
 
 
@@ -91,18 +116,22 @@ class CopilotWidget(BoxLayout):
 
 
 class SpeedWidget(BoxLayout):
+    speed = NumericProperty(veh_speed)
     pass
 
 
 class SignalStrengthWidget(BoxLayout):
+    signal = NumericProperty(net_signal)
     pass
 
 
 class EngineTempWidget(BoxLayout):
+    temp = NumericProperty(eng_temp)
     pass
 
 
 class FuelConsumptionWidget(BoxLayout):
+    fuel_flow = NumericProperty(eng_fuel_flow)
     pass
 
 
@@ -115,11 +144,14 @@ class ExitWidget(BoxLayout):
     pass
 
 
-
-
 # Creating App
 class EVOSDashboardApp(App):
     # lc.handle()
+
+    # Kivy Variables
+    ailerons_font = '../resources/fonts/ailerons/Ailerons-Typeface.otf'
+    up_arrow = '../resources/images/up_arrow.png'
+
     def build(self):
         return Rows()
 
