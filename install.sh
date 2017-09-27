3c4fe6b5b7a3bf64f896b9165c6e0454e96f2ab8
nick <nic.phay@gmail.com>
2017-09-27 16:13:52 -0500

    Removed typo

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo apt -y install build-essential
 sudo apt -y install libglib2.0-dev
 
 
-wget hhttps://github.com/lcm-proj/lcm/releases/download/v1.3.1/lcm-1.3.1.zip
+wget https://github.com/lcm-proj/lcm/releases/download/v1.3.1/lcm-1.3.1.zip
 tar -xzvf lcm-1.3.1.zip
 cd lcm-1.3.1
 ./configure
@@ -18,8 +18,8 @@ make
 sudo make install
 sudo ldconfig
 
-export LCM_INSTALL_DIR=/usr/local/lib
-sudo bash -c 'echo '/usr/local/lib' > /etc/ld.so.conf.d/lcm.conf'
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
-sudo bash -c 'echo "/usr/local/lib/python2.7/site-packages" > /usr/local/lib/python2.7/lcm.pth'
+#export LCM_INSTALL_DIR=/usr/local/lib
+#sudo bash -c 'echo '/usr/local/lib' > /etc/ld.so.conf.d/lcm.conf'
+#export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
+#sudo bash -c 'echo "/usr/local/lib/python2.7/site-packages" > /usr/local/lib/python2.7/lcm.pth'
 
